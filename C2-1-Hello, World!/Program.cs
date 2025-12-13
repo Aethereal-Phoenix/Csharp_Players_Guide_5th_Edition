91ffbcaf35720f7ccffbbc31d7c34bec0fd61fd7
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2025-12-13 03:04:15 -0600

    initial commit, wrote program with graceful exit and exception handling

---
+++ C2-1-Hello, World!/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace C2_1_Hello__World_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Hello, World!");
+                Console.ReadLine();
+                bool exit = true;
+
+                while (exit)
+                {
+                    char exitYN;
+                    Console.WriteLine("Would you like to see it again? Y/N");
+                    try
+                    {
+                        exitYN = char.ToUpper(char.Parse(Console.ReadLine()));
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                        Console.WriteLine();
+                        Console.WriteLine("Please enter a valid response of either Y or N");
+                        Console.ReadLine();
+                        break;
+                    }
+
+
+                    if (exitYN == 'N')
+                    {
+                        return;
+                    }
+                    else if (exitYN == 'Y')
+                    {
+                        break;
+                    }
+                    else if (!(exitYN == 'Y') && !(exitYN == 'N'))
+                    {
+                        Console.WriteLine("Please enter a valid response.");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
