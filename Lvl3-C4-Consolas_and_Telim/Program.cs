dbfc5ba732594cdd92b7bbdd8cf2cba8313c2d3d
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2025-12-13 12:03:00 -0600

    initial commit

---
+++ Lvl3-C4-Consolas_and_Telim/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lvl3_C4_Consolas_and_Telim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("There is a loaf of bread ready.");
+                Console.Write("What is the name of the customer:");
+                string customerName = Console.ReadLine();
+                Console.WriteLine($"{customerName} has been logged as having received their loaf of bread.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
