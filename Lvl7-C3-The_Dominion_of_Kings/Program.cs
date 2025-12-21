075cf5e0f7459fc4b0dc4311d5268eed5c444923
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2025-12-21 03:05:16 -0600

    Working on methods for the program

---
+++ Lvl7-C3-The_Dominion_of_Kings/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lvl7_C3_The_Dominion_of_Kings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
