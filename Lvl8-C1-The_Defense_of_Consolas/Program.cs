eb06e24d625215b404f5e5ca328c69563f79ed9b
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2025-12-22 02:31:09 -0600

    Need to reread the chapter

---
+++ Lvl8-C1-The_Defense_of_Consolas/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Lvl8_C1_The_Defense_of_Consolas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "The Defense of Consolas";
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
