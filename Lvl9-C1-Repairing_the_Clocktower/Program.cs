a623d7fac5a51eccd10d0050ea37647be960ca62
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2026-01-23 10:35:39 -0600

    Complete

--- Lvl9-C1-Repairing_the_Clocktower/Program.cs
+++ Lvl9-C1-Repairing_the_Clocktower/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Let's repair the last part of the clock tower.");
+                Console.WriteLine("Please enter a number and we will assign it either tick or tock");
+                if (!int.TryParse(Console.ReadLine(), out int input))
+                {
+                    Console.WriteLine("You have entered an invalid option. Please try again.");
+                    Console.ReadKey(true);
+                    continue;
+                }
+
+                if(IsEven(input))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Tick");
+                    Console.ReadKey(true);
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Tock");
+                    Console.ReadKey(true);
+                }        
+            }
+        }// End of Main method
+         // Methods go below this line
+
+        public static bool IsEven(int x)
+        {
+            while (true)
+            {
+                if (x % 2 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
-    }
-}
+     // Methods go above this line
+    }// End of Program Class
+}// End of Namespace
