b2b1ceab7c1532e505171733941dba982bc3b67b
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2026-02-02 01:49:13 -0600

    Finished

---
+++ Lvl9-C2-Watchtower/Lvl9-C2-Watchtower/MethodCodex.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Lvl9_C2_Watchtower
+{
+    internal class MethodCodex
+    {
+        public static bool ConfirmInput(string message, char trueChar, char falseChar)// Method to confirm a users choice
+        {
+            trueChar = char.ToUpper(trueChar);
+            falseChar = char.ToUpper(falseChar);
+            while (true) // Will repeat the request until a valid reponse is entered
+            {
+                Header();
+                Console.WriteLine($"{message} {trueChar}/{falseChar}"); // Displays the message string followed by the entered true and false chars
+
+                if (!char.TryParse(Console.ReadLine().ToUpper(), out char input) || (input != trueChar && input != falseChar)) // Checks to see if the users input can be parsed to a char and outputs a valid response to input, also checks to see if the parsed input does not match trueChar or falseChar
+                {
+                    // If the user did not enter a char
+                    Header();
+                    Console.WriteLine("Unfortunately that is not a valid input. Please try again.");
+                    Console.ReadKey(true);
+                    continue;
+                }
+                else if (input == trueChar) // If users input matches trueChar
+                {
+                    return true;
+                }
+                else if (input == falseChar) // If users input matches falseChar
+                {
+                    return false;
+                }
+            }
+        }// End of ConfrimInput method
+
+        public static int Menu(string[] menu)// Make then menu in the main program flow and pass it through the method as an argument
+        {
+            while (true)
+            {
+                Header();
+                for (int i = 0; i < menu.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1} {menu[i]}");
+                }
+
+                if ((!int.TryParse(Console.ReadLine().Trim(), out int response)) || (response < 0 || response > menu.Length))
+                {
+                    Console.WriteLine("You have entered and invalid input, please try again.");
+                    Console.ReadKey(true);
+                    continue;
+                }
+                else
+                {
+                    return response - 1;
+                }
+            }
+        }// End of Menu method
+
+        public static void Header()
+        {
+            int headerWidth = (Program.headerTitle.Length > 20 ? Program.headerTitle.Length + 20 : 40 + (Program.headerTitle.Length % 2));// if headerTitle,Length > 20 set headerWidth to titleLength + 20 else set headerWidth to 40 and if headerTitleLength is odd add another 1 (Using a ternary operator)
+            int headerWhiteSpaceNum = ((headerWidth - 4) - Program.headerTitle.Length) / 2;// Sets the left and right side width of the header for the title line
+
+            Console.Clear();
+            Console.WriteLine(new string('*', headerWidth));// Writes to the console a number of *s = headerWidth
+            Console.WriteLine($"{new string('*', headerWhiteSpaceNum)}  {Program.headerTitle}  {new string('*', headerWhiteSpaceNum)}");
+            Console.WriteLine(new string('*', headerWidth) + "\n\n");
+        }// End of Header method
+
+    }// End of Method Codex Class
+}
---
+++ Lvl9-C2-Watchtower/Lvl9-C2-Watchtower/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Lvl9_C2_Watchtower
+{
+    internal class Program
+    {
+        public static string headerTitle = "Watchtower";
+        static void Main(string[] args)
+        {
+           while (true)
+            {
+                MethodCodex.Header();
+                int xCoord = CollectCoordinate('X');
+                int yCoord = CollectCoordinate('Y');
+
+                bool confirmCoordinate = MethodCodex.ConfirmInput($"The enemy is at ({xCoord}, {yCoord})\nIs this correct?", 'Y', 'N');
+                if (!confirmCoordinate)
+                {
+                    MethodCodex.Header();
+                    Console.WriteLine("Very well. Please try again.");
+                    Console.ReadKey(true);
+                    continue;
+                }
+
+                MethodCodex.Header();
+                Console.WriteLine($"The target is at ({xCoord}, {yCoord}).");
+                Console.ReadKey(true);
+                break;
+            }
+        }// End of Main method
+        // Methods go below this line
+
+        public static int CollectCoordinate(char coord)
+        {
+            while (true)
+            {
+                MethodCodex.Header();
+                Console.WriteLine($"Please enter the {coord} coordinate of the enemy.");
+                
+                if (!int.TryParse(Console.ReadLine(), out var inputCoord))
+                {
+                    MethodCodex.Header();
+                    Console.WriteLine("You have entered an invalid option.\nPlease try again.");
+                    Console.ReadKey(true);
+                    continue;
+                }
+
+                bool confirmInput = MethodCodex.ConfirmInput($"You have entered {inputCoord} as the {coord} coordinate.\nIs this correct?", 'Y', 'N');
+                if (confirmInput)
+                {
+                    return inputCoord;
+                }
+                else
+                {
+                    MethodCodex.Header();
+                    Console.WriteLine("zxzx");
+                } 
+            }
+        }
+       
+        // Methods go above this line
+    }// End of Program class
+}
