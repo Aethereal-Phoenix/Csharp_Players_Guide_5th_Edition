ca243e7246ea1c2743d1632023b28977e65ae78c
Aethereal_Phoenix <a.lee.sprague@gmail.com>
2026-02-04 10:36:51 -0600

    Finished

---
+++ Lvl10-C1-Buying_Inventory/Lvl10-C1-Buying_Inventory/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Lvl10_C1_Buying_Inventory
+{
+    internal class Program
+    {
+        public static string headerTitle = "Buying Inventory";
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string[] menuList = { "Rope", "Torches", "Climbing Gear", "Clean Water", "Machete", "Canoe", "Food Supplies" };
+
+                MethodCodex.Header();
+                Console.WriteLine("The Following items are available");
+                Console.WriteLine("Please select which one you would like to see the price of");
+                int response = MethodCodex.ArrowMenu(menuList);
+                int itemPrice = 0;
+                itemPrice = response switch
+                {
+                    0 => 10,
+                    1 => 16,
+                    2 => 24,
+                    3 => 2,
+                    4 => 20,
+                    5 => 200,
+                    6 => 2
+                };
+
+                Console.WriteLine($"{menuList[response]} will cost {itemPrice} each.");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
