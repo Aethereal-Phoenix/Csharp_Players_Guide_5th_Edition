d99a2c690f0969985b605009d3f0cd0c29640298
Alexander Sprague <a.lee.sprague@gmail.com>
2026-08-11 05:10:17 -0500

    Finished

--- Lvl13_C1_Taking_a_Number/Lvl13_C1_Taking_a_Number/Program.cs
+++ Lvl13_C1_Taking_a_Number/Lvl13_C1_Taking_a_Number/Program.cs
@@ -5,7 +5,53 @@
         public static string headerTitle = "Taking a Number";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MethodCodex.Header();
+
+            int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+            int total = 0;
+            int lowest = array[0];
+            foreach (int i in array)
+            {
+                if (i < lowest) lowest = i;
+                total += i;
+            }
+            float average = (float)total / array.Length;
+            Console.WriteLine(lowest);
+            Console.WriteLine(average);
+
+            static int AskForNumber(string text)
+            {
+                while (true)
+                {
+                    MethodCodex.Header();
+
+                    Console.WriteLine(text);
+
+                    int.TryParse(Console.ReadLine(), out int response);
+
+                    if (MethodCodex.ConfirmInput($"Is {text} your final answer?", 'Y', 'N'))
+                    {
+                        return response;
+                    }
+                }
+            }
+
+            static int AskForNumberInRange(string text, int min, int max)
+            {
+                while (true)
+                {
+                    MethodCodex.Header();
+
+                    Console.WriteLine($"{text} \nMin:{min} \nMax:{max}");
+
+                    int.TryParse(Console.ReadLine(), out int answer);
+
+                    if (MethodCodex.ConfirmInput($"You entered {answer}. Is this correct?", 'Y', 'N') && !(answer < min || answer > max))
+                    {
+                        return answer;
+                    }
+                }
+            }
         }
     }
 }
