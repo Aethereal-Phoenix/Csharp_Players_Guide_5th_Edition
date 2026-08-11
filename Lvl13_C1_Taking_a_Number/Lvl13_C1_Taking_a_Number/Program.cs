34af8524ef6c066380f0c9c2ab2d3fc6ed7d34f0
Alexander Sprague <a.lee.sprague@gmail.com>
2026-08-11 04:15:12 -0500

    Initial Commit

---
+++ Lvl13_C1_Taking_a_Number/Lvl13_C1_Taking_a_Number/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lvl13_C1_Taking_a_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
