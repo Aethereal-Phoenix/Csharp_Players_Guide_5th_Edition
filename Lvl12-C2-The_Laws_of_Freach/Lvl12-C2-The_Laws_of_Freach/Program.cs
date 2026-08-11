bf704ea329b18b43de39c2366c31bfa8f516ee07
Alexander Sprague <a.lee.sprague@gmail.com>
2026-08-11 04:06:42 -0500

    level done

---
+++ Lvl12-C2-The_Laws_of_Freach/Lvl12-C2-The_Laws_of_Freach/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Lvl12_C2_The_Laws_of_Freach
+{
+    internal class Program
+    {
+        public static string headerTitle = "The Laws of Freach";
+        static void Main(string[] args)
+        {
+            MethodCodex.Header();
+
+            int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+            int total = 0;
+            int lowest = array[0];
+            foreach (int i in array) {
+                if (i < lowest) lowest = i;
+                total += i;
+            }
+            float average = (float)total / array.Length;
+            Console.WriteLine(lowest);
+            Console.WriteLine(average);
+        }
+    }
+}
