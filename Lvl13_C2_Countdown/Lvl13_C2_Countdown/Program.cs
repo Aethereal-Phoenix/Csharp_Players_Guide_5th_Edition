72f9d9e71860cec2030bf7b9b8db2e32c6b46417
Alexander Sprague <a.lee.sprague@gmail.com>
2026-08-11 05:28:55 -0500

    Finished challenge

---
+++ Lvl13_C2_Countdown/Lvl13_C2_Countdown/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Lvl13_C2_Countdown
+{
+    internal class Program
+    {
+        public static string headerTitle = "Countdown";
+        static void Main(string[] args)
+        {
+            MethodCodex.Header();
+
+            Countdown(100);
+        }
+        static void Countdown(int num)
+        {
+            Console.WriteLine(num);
+            if (num > 1) Countdown(num - 1);
+        }
+    }
+}
